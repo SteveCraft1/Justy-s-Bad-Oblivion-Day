84f98729466681affc74d7bfaf7eb538053835c1
SteveCraft1 <73554591+SteveCraft1@users.noreply.github.com>
2021-07-02 18:23:31 -0300

    Fixed Justy's wrong script command for health

--- data/scripts/update.c
+++ data/scripts/update.c
@@ -8,7 +8,7 @@ void main()
 	if(openborvariant("in_level")==1){
 		if(P1 && P1name == "Justy"){
 			int HP1 = getentityproperty(P1, "health");
-			drawbox(33,5,62,11,1500,rgbcolor(255,135,0),0);
+			drawbox(33,5,62,11,HP1-3,rgbcolor(255,135,0),0);
 			drawsprite(Bar, 3, 3, 1000);
 			int JustyAMMO = getentityproperty(P1, "mp");
 			drawstring (109, 12, 2, JustyAMMO);
